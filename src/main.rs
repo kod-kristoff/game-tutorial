66449b8a507172951991293874a12198ebf496a6
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-02-11 12:34:08 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::render::{Texture,WindowCanvas};
+use sdl2::image::{self, LoadTexture, InitFlag};
+use std::time::Duration;
+
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    canvas.copy(texture, None, None)?;
+
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("game tutorial", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make canvas");
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        // Handle events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        // Time management
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
+}
